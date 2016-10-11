b096ac2e3d4fa8aed0a299d0b6826c2b36c0e11d
Mcginn <958032738@qq.com>
2016-10-11 18:58:20 +0800

    ''

--- 模板/gauss_linear_base.cpp
+++ 模板/gauss_linear_base.cpp
@@ -15,3 +15,22 @@ void gauss() {
 				a[j] ^= a[i];
 	}
 }
+
+struct Gauss {
+	int cnt;
+	ll st, lb[66];
+	void init() {
+		cnt = 0, st = 0;
+		rep(i, 0, 61) lb[i] = 0;
+	}
+	void add(ll w) {
+		st |= w;
+		for (int i = 60; i >=0; --i) {
+			if ((w >> i & 1) && !lb[i]) {
+				lb[i] = w, ++cnt;
+				break;
+			}
+			w = min(w, w ^ lb[i]);
+		}
+	}
+} G;
