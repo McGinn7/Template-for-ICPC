c5e0fa20384ae29e7ce9f52c8771ae4691ceffe6
Mcginn <958032738@qq.com>
2016-12-21 23:14:12 +0800

    ''

--- 模板/java.java
+++ 模板/java.java
@@ -6,11 +6,19 @@ public class Main {
 	static final String file = "data";
 
 	public static void main(String[] args) {
+		/**
+		 * 大数常用操作
+		 * add、subtract、multiply、divide、mod、pow、modPow
+		 * modInverse(BigInteger mod) - 逆元，需互质
+		 */
 		Scanner in = null;
 		PrintWriter out = null;
 		try {
+			// 标准输入输出
+			in = new Scanner(System.in);
+			System.out.println("out content");
+			// 文件输入输出
 			in = new Scanner(new File(file + ".in"));
-			// in = new Scanner(System.in);
 			out = new PrintWriter(new File(file + ".out"));
 		} catch (Exception e) {
 			e.printStackTrace();
