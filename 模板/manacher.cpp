f98fedb4da13070360ce2287ceaac20ba39c3391
Mcginn <mcginn2013@outlook.com>
2017-04-02 20:16:21 +0800

    .

--- 模板/manacher.cpp
+++ 模板/manacher.cpp
@@ -1,3 +1,15 @@
+void manacher(int n, int a[], int l[]) { // l[] length = 2 * n
+	l[0] = 1;
+	for (int i = 1, j = 0; i + 1 < n << 1; ++i) {
+		int p = i >> 1, q = i - p, r = (j + 1 >> 1) + l[j] - 1;
+		l[i] = r < q ? 0 : min(r - q + 1, l[(j << 1) - i]);
+		while (p - l[i] != -1 && q + l[i] != n && a[p - l[i]] == a[q + l[i]])
+			++l[i];
+		if (q + l[i] - 1 > r)
+			j = i;
+	}
+}
+//------------------------
 int p[N];
 void manacher(int n, char a[]) { // 1 ~ n, #a#b#d#a#
 	int id, mx = 0;
@@ -9,3 +21,5 @@ void manacher(int n, char a[]) { // 1 ~ n, #a#b#d#a#
 			id = i, mx = i + p[i];
 	}
 }
+
+
