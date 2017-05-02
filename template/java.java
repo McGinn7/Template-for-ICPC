cece64f59d0fc13b3142689a90ee288204298092
mcginn7 <958032738@qq.com>
2017-05-02 11:29:50 +0800

    .

--- template/java.java
+++ template/java.java
@@ -1,32 +1,35 @@
 import java.io.File;
 import java.io.PrintWriter;
-import java.util.Scanner;
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		/**
-		 * 大数常用操作
-		 * add、subtract、multiply、divide、mod、pow、modPow
-		 * modInverse(BigInteger mod) - 逆元，需互质
-		 */
+
+	/**
+	 * 大数常用操作
+	 * add、subtract、multiply、divide、mod、pow、modPow
+	 * modInverse(BigInteger mod) - 逆元，需互质
+	 */
+
+	Main() throws Exception {
 		Scanner in = null;
 		PrintWriter out = null;
-		try {
-			// 标准输入输出
-			in = new Scanner(System.in);
-			// 文件输入输出
-			in = new Scanner(new File("input.txt"));
-			out = new PrintWriter(new File("output.txt"));
-			// in.nextInt(), in.nextBigInteger() 
-			// System.out.println([String]), out.println([String])
-		} catch (Exception e) {
-		}
+		// 标准输入输出
+		in = new Scanner(System.in);
+		out = new PrintWriter(System.out);
+		// 文件输入输出
+		in = new Scanner(new File("input.txt"));
+		out = new PrintWriter(new File("output.txt"));
 		/**
-		 * note: must close file!
+		 * in.next[Type]() , Type = Int, Long, Double, BigInteger
+		 * out.println([String])
 		 */
-		in.close();
-		out.close();
+		// Note: must close file!
+		in.close();out.close();
+	}
+
+	public static void main(String[] args) throws Exception {
+		new Main();
 	}
 }
