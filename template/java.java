4db2c5998f2a747c1116c398926d21fd8e7789a7
Mcginn <mcginn2013@outlook.com>
2017-09-24 10:32:14 +0800

    add functions about BigDecimal

--- template/java.java
+++ template/java.java
@@ -7,9 +7,24 @@ import java.util.Scanner;
 public class Main {
 
 	/**
+	* Compile: javac Main.java
+	* Run: java Main 
+	*/
+
+	/**
 	 * 大数常用操作
 	 * add、subtract、multiply、divide、mod、pow、modPow
+	 * BigInteger[] divideAndRemainder(BigInteger val)
+	 * BigInteger modPow(BigInteger exponent, BigInteger m)
 	 * modInverse(BigInteger mod) - 逆元，需互质
+	 */
+
+	/**
+	 * 大浮点数
+	 * BigDecimal divide(BigDecimal divisor, int scale, RoundingMode roundingMode)
+	 * RoundingMode: ROUND_HALF_[DOWN|UP|EVEN], ROUND_FLOOR, ROUND_CEILING
+	 * BigDecimal negate(): return value is (-this)
+	 * BigDecimal setScale(int newScale, [int roundingMode]): 保留小数位数
 	 */
 
 	Main() throws Exception {
@@ -25,6 +40,10 @@ public class Main {
 		 * in.next[Type]() , Type = Int, Long, Double, BigInteger
 		 * out.println([String])
 		 */
+		// multiple testcases
+		while (in.hasNext()) {
+			// TODO
+		}
 		// Note: must close file!
 		in.close();out.close();
 	}
