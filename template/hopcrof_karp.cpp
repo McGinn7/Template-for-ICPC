7475ec8ae90f2f779639631dc91da4d78f76f7f3
Mcginn <mcginn2013@outlook.com>
2017-12-08 21:56:09 +0800

    remove x, y

--- template/hopcrof_karp.cpp
+++ template/hopcrof_karp.cpp
@@ -3,34 +3,31 @@
 // Right size - m, numbered from 1 to m
 struct Hopcroft {
 	int n, m;
-	int que[N], dx[N], dy[N], matx[N], maty[N];
+	int que[N], dw[N << 1], mat[N << 1];
 	vector<int> e[N];
 	void init(int _n, int _m) {
 		n = _n, m = _m;
 		rep(i, 1, n + 1) e[i].clear();
-		rep(i, 1, n + 1) matx[i] = 0;
-		rep(i, 1, m + 1) maty[i] = 0;
+		rep(i, 1, n + m + 1) mat[i] = 0;
 	}
 	void addEdge(int u, int v) {
-		e[u].pb(v);
+		e[u].pb(n + v);
 	}
 	int bfs() {
 		int flag = 0, qh = 0, qt = 0;
-		rep(i, 1, m + 1) dy[i] = 0;
-		rep(i, 1, n + 1) {
-			dx[i] = 0;
-			if (!matx[i]) que[qt++] = i;
-		}
+		rep(i, 1, n + m + 1) dw[i] = 0;
+		rep(i, 1, n + 1) if (!mat[i]) 
+			que[qt++] = i;
 		while (qh < qt) {
 			int u = que[qh++];
 			rep(i, 0, sz(e[u])) {
 				int v = e[u][i];
-				if (dy[v]) continue;
-				dy[v] = dx[u] + 1;
-				if (!maty[v]) flag = true;
+				if (dw[v]) continue;
+				dw[v] = dw[u] + 1;
+				if (!mat[v]) flag = true;
 				else {
-					dx[maty[v]] = dx[u] + 2;
-					que[qt++] = maty[v];
+					dw[mat[v]] = dw[u] + 2;
+					que[qt++] = mat[v];
 				}
 			}
 		}
@@ -39,10 +36,10 @@ struct Hopcroft {
 	int dfs(int u) {
 		rep(i, 0, sz(e[u])) {
 			int v = e[u][i];
-			if (dy[v] != dx[u] + 1) continue;
-			dy[v] = 0;	
-			if (!maty[v] || dfs(maty[v])) {
-				matx[u] = v, maty[v] = u;
+			if (dw[v] != dw[u] + 1) continue;
+			dw[v] = 0;
+			if (!mat[v] || dfs(mat[v])) {
+				mat[u] = v, mat[v] = u;
 				return true;
 			}
 		}
@@ -50,7 +47,7 @@ struct Hopcroft {
 	}
 	int run() {
 		int ret = 0;
-		while (bfs()) rep(i, 1, n + 1) if (!matx[i])
+		while (bfs()) rep(i, 1, n + 1) if (!mat[i])
 				ret += dfs(i);
 		return ret;
 	}
