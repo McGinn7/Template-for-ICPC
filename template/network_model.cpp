fb88016ac6e3c61a3b3ea589ba9b86efa512b16d
Chen Mingqin <958032738@qq.com>
2020-04-19 15:40:36 +0800

    update the description of network flow model

--- template/network_model.cpp
+++ template/network_model.cpp
@@ -1,28 +1,42 @@
-/*
-	无源汇上下界最大流
-	du[i] = in[i] - out[i]
-	du[i] > 0, add edge S -> i(cap = du[i])
-	du[i] < 0, add edge i -> T(cap = -du[i])
-	if maxflow(S, T) = sum{du[i]}, du[i] > 0, 
-	exist a feasible solution.
+无源汇上下界最大流
+define 
+	virtual source S, virtual sink T
+construct 
+	du[i] = must_in[i] - must_out[i]
+	addEdge(S, i, cap = du[i]) if du[i] > 0	
+	addEdge(i, T, cap = -du[i]) if du[i] < 0
+result
+	if maxflow(S, T) == sum(du[i]) with du[i] > 0
+	then exist a feasible solution.
 
-	有源汇上下界最大流
-	source S, sink T, super source SS, super sink TT
-	add edge T -> S(cap = INF), change to 无源汇网络流
-	1. get flow1 = maxflow(SS, TT)
-	2. if exist a feasible solution, get flow2 = maxflow(S, T)
-	answer = flow1 + flow2
+有源汇上下界最大流
+define
+	source S, sink T, virtual source SS, virtual sink TT	
+construct
+	addEdge(T, S, cap = INF)	
+	(the remaining steps are the same as 无源汇上下界最大流)		
+result
+	1. there must be a feasible solution from SS to TT.
+	2. answer = maxflow(SS, TT)	+ maxflow(S, T) // don't chnage order
 
-	有源汇上下界最小流
-	source S, sink T, super source SS, super sink TT
+有源汇上下界最小流
+define 
+	source S, sink T, virtual source SS, virtual sink TT
+construct
+	(first, view problem as 无源汇上下界最大流)
+	addEdge(T, S, cap = INF) after get maxflow(SS, TT)	
+result
 	1. get maxflow(SS, TT)
-	2. add edge T -> S(cap = INF)
-	3. get maxflow(SS, TT)
-	if all flow of edge S->i equal to 0, exist solution and
-		the answer = flow[S->T]
-
-	最大权闭合子图
-	weight[v] > 0, add edge S -> v(cap = weight[v])
-	weight[v] < 0, add edge v -> T(cap = -weight[v])
-	answer = sum(weight[v]) - maxflow(S, T), weight[v] > 0
-*/
+	2. addEdge(T, S, cap = INF)
+	3. get maxflow(SS, TT) again
+	4. if all S->i are full, there exits a solution and the answer 
+	   equals to flow(S, T) now
+		
+最大权闭合子图
+define
+	source S, sink T	
+construct
+	addEdge(S, i, cap = weight[i]) if weight[i] > 0
+	addEdge(i, T, cap = -weight[i]) if weight[i] < 0	
+result
+	answer = sum(weight[i] > 0) with weight[i] > 0 - maxflow(S, T)
